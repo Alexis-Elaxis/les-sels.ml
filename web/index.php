0a77a2a3b7e7ddca66eab6673084556c31d5ce22
Alexis <52383750+Alexis-Elaxis@users.noreply.github.com>
2022-11-13 11:34:20 +0100

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+header('Location: /home');
+?>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Les-sels.ml</title>
+
+    <meta property="og:image:url" content="https://www.epicerie-pro.com/images/Image/https-www-epicerie-pro-com-images-Image-sel-dosette-1.jpg">
+    <meta property="og:title" content="L'entonnoir à Sel" />
+    <meta property="og:description" content="On est sur la Sel-lette, ta pigé ? Viens collecter du Sel avec nous ;)" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://les-sels.ml/" />
+</head>
+</html>
